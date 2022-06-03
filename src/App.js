74f08cb9dc0d58d3e085d93c20fc61ed9148072a
ashish kataria <ashishkataria002@gmail.com>
2022-06-04 01:42:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import NavBar from "./components/NavBar.js";
 import {BrowserRouter as Router} from "react-router-dom"
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+import Heading  from "./components/Heading.js"
+
+
+
 
 
 function App() { //functional component
@@ -13,6 +18,8 @@ function App() { //functional component
         <PreNavBar/>
         <NavBar/>
         <Slider start={data.banner.start}/>
+        <Offers offer={data.offer}/>
+        <Heading text="STAR PRODUCTS"/>
     </Router>
   );
 }

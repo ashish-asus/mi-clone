149e3d3f9d02ebde550bc953109f5cd74e63eab5
ashish kataria <ashishkataria002@gmail.com>
2022-06-04 01:13:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import PreNavBar from "./components/PreNavBar";
 import NavBar from "./components/NavBar.js";
+import {BrowserRouter as Router} from "react-router-dom"
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+
+
 function App() { //functional component
 
   return (
-    <div>  
+    <Router>
         <PreNavBar/>
         <NavBar/>
-    </div>
+        <Slider start={data.banner.start}/>
+    </Router>
   );
 }
 
